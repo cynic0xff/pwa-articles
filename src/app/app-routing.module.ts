35c914e330fae879fbf88d7a0b86baa7afcdd54b
Scott Wren <scott.wren@nrtsoutheast.com>
2019-06-11 13:05:36 -0400

    Add js articles html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesTechnologyComponent } from './articles-technology/articles-technology.component';
+import { ArticlesJavascriptComponent } from './articles-javascript/articles-javascript.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticlesTechnologyComponent },
+  { path: 'articles-javascript', component: ArticlesJavascriptComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
